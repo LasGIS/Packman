75bc54bafa6b3da18d4c2ebed1ba7d53667535d2
LasGIS <laskin@yandex.ru>
2016-03-30 17:36:40 +0600

    Simplify Developing.

--- simplify/src/main/java/fkn/dlaskina/packman/panels/ConfigPanel.java
+++ simplify/src/main/java/fkn/dlaskina/packman/panels/ConfigPanel.java
@@ -3,6 +3,8 @@ package fkn.dlaskina.packman.panels;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -30,37 +32,64 @@ public class ConfigPanel extends JPanel {
     /** всего бонусов */
     private JLabel lblBonus = new JLabel("0", JLabel.LEFT);
     /** собранных бонусов */
-    private JLabel lblBonusMax = new JLabel("20", JLabel.LEFT);
+    private JLabel lblBonusMax = new JLabel("null", JLabel.LEFT);
 
     /**
      * Конструктор.
+    final String[] fontNames = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+    LOG.info(fontNames);
      */
     public ConfigPanel() {
         super();
 
         final JPanel controlPanel = new JPanel(new GridBagLayout());
-        controlPanel.setBackground(MapPanel.PANEL_GRAY_COLOR);
         final GridBagConstraints c = new GridBagConstraints(0, 0, 1, 1, 0, 0,
-            CENTER, BOTH, new Insets(2, 5, 2, 4), 0, 0);
+            CENTER, BOTH, new Insets(2, 10, 2, 10), 0, 0);
 
-        controlPanel.add(new JLabel("Призы", JLabel.CENTER), c);
+        final Insets mainInsets = new Insets(2, 10, 2, 10);
+        final Insets deskInsets = new Insets(2, 10, 2, 2);
+        final Insets digitInsets = new Insets(2, 5, 2, 10);
+        final Font mainFont = new Font("Arial", Font.BOLD, 14);
+        final Font deskFont = new Font("Arial", Font.PLAIN, 12);
+        final Font digitFont = new Font("Courier New", Font.BOLD, 12);
+
+        c.gridwidth = 2;
+        c.insets = mainInsets;
+        JLabel label = new JLabel("Призы:", JLabel.CENTER);
+        label.setFont(mainFont);
+        label.setForeground(Color.BLUE);
+        controlPanel.add(label, c);
+
         c.gridy = 1;
-        controlPanel.add(new JLabel("всего:", JLabel.RIGHT), c);
+        c.gridx = 0;
+        c.gridwidth = 1;
+        c.insets = deskInsets;
+        label = new JLabel("всего:", JLabel.RIGHT);
+        label.setFont(deskFont);
+        controlPanel.add(label, c);
+
         c.gridx = 1;
+        c.insets = digitInsets;
+        lblBonusMax.setFont(digitFont);
+        lblBonusMax.setForeground(Color.RED);
         controlPanel.add(lblBonusMax, c);
+
         c.gridx = 0;
         c.gridy = 2;
-        controlPanel.add(new JLabel("получено", JLabel.RIGHT), c);
+        c.insets = deskInsets;
+        label = new JLabel("получено:", JLabel.RIGHT);
+        label.setFont(deskFont);
+        controlPanel.add(label, c);
+
         c.gridx = 1;
+        c.insets = digitInsets;
+        lblBonus.setFont(digitFont);
+        lblBonus.setForeground(Color.RED);
         controlPanel.add(lblBonus, c);
 
         setLayout(new BorderLayout());
-        add(controlPanel, BorderLayout.CENTER);
+        add(controlPanel, BorderLayout.NORTH);
         singleton = this;
-    }
-
-    public static ConfigPanel getConfigPanel() {
-        return singleton;
     }
 
     /**
