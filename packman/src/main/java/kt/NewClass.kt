6765a8d7bc6f85fe4b7563832e800b799cc503f9
Vladimir Laskin <VLaskin@luxoft.com>
2019-11-26 20:10:29 +0600

    Kotlin: добавил Kotlin в проект

---
+++ packman/src/main/java/kt/NewClass.kt
@@ -0,0 +1,10 @@
+package kt
+
+/**
+ * <description>
+ *
+ * @author VLaskin
+ * @since <pre>26.11.2019</pre>
+ */
+class NewClass {
+}
---
+++ packman/src/main/java/kt/app.kt
@@ -0,0 +1,15 @@
+package kt
+
+/**
+ * <description>
+ *
+ * @author VLaskin
+ * @since <pre>26.11.2019</pre>
+ */
+fun main(args: Array<String>) {
+    print("Kotlin: \"")
+    args.forEach {
+        print("$it ")
+    }
+    print("\"!")
+}
